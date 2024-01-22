4b82cab8106425e9f898ff3bbcd031b169d6b7e6
jOsterrielt <jorgeosterrielt@gmail.com>
2024-01-22 02:07:49 -0300

    rest-api-for-deploy

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const crypto = require("node:crypto");
+const movies = require("./movies.json");
+const { validateMovie, validatepartialMovie } = require("./schema/movies");
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:4321",
+        "https://movies.com",
+      ];
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+app.disable("x-powered-by");
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hola mundo" });
+});
+
+app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+  let responseMovies = movies;
+  if (genre) {
+    responseMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+  }
+  res.json(responseMovies);
+});
+
+app.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieFound = movies.find((movie) => movie.id === id);
+  res.json(movieFound);
+});
+
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    res.status(400).json({
+      error: JSON.parse(result.error.message),
+    });
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data,
+  };
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
+});
+
+app.patch("/movies/:id", (req, res) => {
+  const result = validatepartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    res.status(404).json({ message: "Movie Not Found" });
+  }
+
+  const updatedMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+  movies[movieIndex] = updatedMovie;
+  return res.json(updatedMovie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  movies.splice(movieIndex, 1);
+
+  return res.json({ message: "Movie deleted" });
+});
+
+const PORT = process.env.PORT ?? 4321;
+
+app.listen(PORT, () => {
+  console.log(`server listening on http://localhost:${PORT}`);
+});
---
+++ schema/movies.js
@@ -0,0 +1,45 @@
+const z = require("zod");
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: "Movie title must be a string",
+    required_error: "Movie title is required",
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(0),
+  poster: z.string().url({
+    message: "Poster must be a valid url",
+  }),
+  genre: z.array(
+    z.enum([
+      "Action",
+      "Adventure",
+      "Comedy",
+      "Drama",
+      "Fantasy",
+      "Horror",
+      "Thriller",
+      "Sci-Fi",
+      "Crime",
+    ]),
+    {
+      required_error: "Movie genre is required",
+      invalid_type_error: "Movie genre must be an Array of enum Genre",
+    }
+  ),
+});
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object);
+}
+
+function validatepartialMovie(object) {
+  return movieSchema.partial().safeParse(object);
+}
+
+module.exports = {
+  validatepartialMovie,
+  validateMovie,
+};
